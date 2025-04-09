46d1335a46618207c82e0851fa775fe580b36bba
Lada Kesseler <archie.kesseler@gmail.com>
2025-04-09 11:50:46 -0700

    Deleted unused imports

--- src/main/java/com/bookstore/service/BookService.java
+++ src/main/java/com/bookstore/service/BookService.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 @Service
 public class BookService {
 
     private final BookRepository bookRepository;
-    
+
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-    
+
     public List<Book> getTop10Books() {
         Pageable pageable = PageRequest.of(0, 10);
         return bookRepository.findTop10Books(pageable);
