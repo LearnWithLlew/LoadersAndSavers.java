d38589e44bfe9f1ca245156a2217b142c1e3c3f6
Lada Kesseler <archie.kesseler@gmail.com>
2025-04-09 13:33:13 -0700

    . t more explicit test
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- src/test/java/com/bookstore/controller/BookControllerDirectTest.java
+++ src/test/java/com/bookstore/controller/BookControllerDirectTest.java
@@ -1,11 +1,15 @@
 package com.bookstore.controller;
 
+import com.bookstore.model.Book;
+import com.spun.util.persistence.Loader;
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.FileLauncherReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
+import org.lambda.functions.Function2;
 import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
@@ -16,6 +20,19 @@ public class BookControllerDirectTest {
     public void testDirectRenderingOfThymeleafTemplateWithLoader() throws Exception {
         var model = new ConcurrentModel();
         String page = BookController.listBooks(model, () -> List.of(BookUtils.getTwilit()));
+        String htmlOutput = ThymeleafUtils.renderPage(page, model);
+        Approvals.verifyHtml(htmlOutput);
+    }
+
+    @Test
+    public void testDirectRendering() throws Exception {
+        Function2<Model, Loader<List<Book>>, String> stuff = BookController::listBooks;
+        verifyHtml(stuff, () -> List.of(BookUtils.getTwilit()));
+    }
+
+    private <P1> void verifyHtml(Function2<Model, P1, String> call, P1 p1) {
+        var model = new ConcurrentModel();
+        String page = call.call(model, p1);
         String htmlOutput = ThymeleafUtils.renderPage(page, model);
         Approvals.verifyHtml(htmlOutput);
     }
