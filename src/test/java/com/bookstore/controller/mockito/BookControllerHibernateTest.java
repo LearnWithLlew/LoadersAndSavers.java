7f6f2622c6f32c8794f87ab349950ec104e3c1da
Lada Kesseler <archie.kesseler@gmail.com>
2025-04-15 15:23:47 -0700

    . t inline
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- src/test/java/com/bookstore/controller/mockito/BookControllerHibernateTest.java
+++ src/test/java/com/bookstore/controller/mockito/BookControllerHibernateTest.java
@@ -3,20 +3,16 @@ package com.bookstore.controller.mockito;
 import com.bookstore.controller.BookController;
 import com.bookstore.controller.ThymeleafUtils;
 import com.bookstore.service.HibernateBookService;
-import org.apache.derby.jdbc.EmbeddedDataSource;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.FileLauncherReporter;
 import org.approvaltests.reporters.MultiReporter;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.ui.ConcurrentModel;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -25,18 +21,6 @@ public class BookControllerHibernateTest {
 
     @Test
     public void testDirectRenderingOfThymeleafTemplate() throws Exception {
-        HibernateBookService hibernateBookService = new HibernateBookService(getDataSource());
-
-        var model = new ConcurrentModel();
-
-        String page = new BookController(null, hibernateBookService).listBooksV3(model);
-        String htmlOutput = ThymeleafUtils.renderPage(page, model);
-
-        Approvals.verifyHtml(htmlOutput,
-            new Options().withReporter(new MultiReporter(DiffReporter.INSTANCE, new FileLauncherReporter())));
-    }
-
-    public DataSource getDataSource() throws SQLException {
         var dataSource = mock(DataSource.class);
         var connection = mock(Connection.class);
         when(dataSource.getConnection()).thenReturn(connection);
@@ -93,7 +77,15 @@ public class BookControllerHibernateTest {
         // wasNull logic for IDs (none are null)
         when(resultSet.wasNull()).thenReturn(false);
 
-        return dataSource;
+        HibernateBookService hibernateBookService = new HibernateBookService(dataSource);
+
+        var model = new ConcurrentModel();
+
+        String page = new BookController(null, hibernateBookService).listBooksV3(model);
+        String htmlOutput = ThymeleafUtils.renderPage(page, model);
+
+        Approvals.verifyHtml(htmlOutput,
+            new Options().withReporter(new MultiReporter(DiffReporter.INSTANCE, new FileLauncherReporter())));
     }
 
 }
